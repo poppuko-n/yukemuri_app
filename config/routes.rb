a18a594a5e01d79466f1ce6ab7e04151def38a71
村上颯太 <murakamisouta@sota.local>
2025-11-14 17:13:07 +0900

    users#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
   get "up" => "rails/health#show", as: :rails_health_check
 end
