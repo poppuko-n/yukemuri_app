4112f9d92d9f1694e4e4a81848afb842ea3a4f67
poppuko-n <souta0609@icloud.com>
2025-11-17 15:45:48 +0900

    未修正　

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -3,6 +3,8 @@
 class Admins::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
 
+  layout 'admin'
+
   # GET /resource/sign_in
   # def new
   #   super
