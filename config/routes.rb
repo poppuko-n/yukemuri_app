fa34846376d512d4fab6bf5c2f338e2fd0b139fd
poppuko-n <souta0609@icloud.com>
2025-11-17 11:29:04 +0900

    管理者登録できるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: { registrations: "admins/registrations", sessions: "admins/sessions" }
   devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
   get "up" => "rails/health#show", as: :rails_health_check
 end
