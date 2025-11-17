2c89153e3f432fa49c10def9e692470d68eabaa2
poppuko-n <souta0609@icloud.com>
2025-11-17 23:40:59 +0900

    ログイン後の遷移を修正　

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -26,4 +26,12 @@ class Admins::SessionsController < Devise::SessionsController
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
+
+  def after_sign_in_path_for(resource)
+    admins_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -26,4 +26,12 @@ class Users::SessionsController < Devise::SessionsController
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
+
+  def after_sign_in_path_for(resource)
+    users_root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_users_session_path
+  end
 end
