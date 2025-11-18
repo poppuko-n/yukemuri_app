a9cd4e967b756e5da529ec5cb6f47b3ccb1a1049
poppuko-n <souta0609@icloud.com>
2025-11-18 15:38:14 +0900

    編集と削除まで

--- app/controllers/admins/accommodations_controller.rb
+++ app/controllers/admins/accommodations_controller.rb
@@ -1,11 +1,10 @@
 class Admins::AccommodationsController < Admins::ApplicationController
+  before_action :set_accommodation, only: %w[show edit update destroy]
   def index
     @accommodations = Accommodation.all
   end
 
-  def show
-    @accommodation = Accommodation.find(params[:id])
-  end
+  def show; end
 
   def new
     @accommodation = Accommodation.new
@@ -14,13 +13,32 @@ class Admins::AccommodationsController < Admins::ApplicationController
   def create
     @accommodation = Accommodation.new(accommodation_params)
     if @accommodation.save
-      redirect_to admins_root_path, notice: t('controller.created')
+      redirect_to :show, notice: t('controller.created')
     else
       render :new, status: :unprocessable_content
     end
   end
 
+  def edit; end
+
+  def update
+    if @accommodation.update(accommodation_params)
+      redirect_to :show, notice: t('controller.updated')
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @accommodation.destroy!
+    redirect_to admins_root_path, notice: t('controller.destroyed')
+  end
+
   private
+
+  def set_accommodation
+    @accommodation = Accommodation.find(params[:id])
+  end
 
   def accommodation_params
     params.require(:accommodation).permit(:name, :prefecture, :address, :phone_number, :category, :image, :description, :published)
