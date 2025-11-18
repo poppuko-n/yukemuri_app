b3fcd5b004ec1bae71efeb061ae39cebe01d9e08
poppuko-n <souta0609@icloud.com>
2025-11-19 06:38:56 +0900

    部屋タイプ詳細

--- app/controllers/admins/accommodations/room_types_controller.rb
+++ app/controllers/admins/accommodations/room_types_controller.rb
@@ -1,4 +1,5 @@
 class Admins::Accommodations::RoomTypesController < Admins::Accommodations::ApplicationController
+  before_action :set_room_type, only: %i[show]
   def new
     @room_type = @accommodation.room_types.build
   end
@@ -12,7 +13,13 @@ class Admins::Accommodations::RoomTypesController < Admins::Accommodations::Appl
     end
   end
 
+  def show; end
+
   private
+
+  def set_room_type
+    @room_type = @accommodation.room_types.find(params[:id])
+  end
 
   def room_type_params
     params.require(:room_type).permit(:name, :capacity, :base_price, :image, :description, :position)
