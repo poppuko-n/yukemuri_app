05375f41b85779759317d5f49fe4a02ad2fb81a8
poppuko-n <souta0609@icloud.com>
2025-11-19 14:51:31 +0900

    ユーザの編集、削除

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,5 +1,19 @@
 class Admins::UsersController < Admins::ApplicationController
+  before_action :set_user, only: %i[show destroy]
   def index
     @users = User.default_order
+  end
+
+  def show; end
+
+  def destroy
+    @user.destroy!
+    redirect_to admins_users_path, notice: t('controller.destroyed'), status: :see_other
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
         resources :room_inventories, only: %i[new create edit update], module: :room_types
       end
     end
-    resources :users, only: %i[index]
+    resources :users, only: %i[index show destroy]
   end
   get "up" => "rails/health#show", as: :rails_health_check
 end
